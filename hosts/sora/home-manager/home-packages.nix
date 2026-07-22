38a049208c2b431857f7577c572f0ab1cd0602d5
Fernando Marques <offrakki@gmail.com>
2026-07-22 20:34:07 -0300

    add PenEcho canvas CLI

--- hosts/sora/home-manager/home-packages.nix
+++ hosts/sora/home-manager/home-packages.nix
@@ -1,4 +1,43 @@
 {pkgs, ...}: let
+  penecho-unwrapped = pkgs.buildNpmPackage {
+    pname = "penecho";
+    version = "0.7.0";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "penecho";
+      repo = "penecho";
+      rev = "067222311e09a1fa3dc26581efb8dbea8fd2d491";
+      hash = "sha256-yDSM8+mq/8/ZGXHaZ6Had+7hEBrCOyQvNbgVHGBdSFQ=";
+    };
+    npmDepsHash = "sha256-t9VoxA5caqz7zNzO1AZJhOPdOQ8/KPhTKl7QYQddEK0=";
+    dontNpmBuild = true;
+
+    nativeBuildInputs = [pkgs.makeWrapper];
+    installPhase = ''
+      runHook preInstall
+      mkdir -p "$out/lib/node_modules/penecho" "$out/bin"
+      cp -r . "$out/lib/node_modules/penecho"
+      makeWrapper ${pkgs.nodejs}/bin/node "$out/bin/penecho-unwrapped" \
+        --add-flags "$out/lib/node_modules/penecho/cli.js"
+      runHook postInstall
+    '';
+
+    meta = {
+      description = "Shared AI canvas for handwriting, equations, and diagrams";
+      homepage = "https://github.com/penecho/penecho";
+      license = pkgs.lib.licenses.agpl3Only;
+      mainProgram = "penecho-unwrapped";
+    };
+  };
+
+  penecho = pkgs.writeShellScriptBin "penecho" ''
+    # Codex refuses helper binaries when PenEcho isolates CODEX_HOME below /tmp.
+    export TMPDIR="''${XDG_CACHE_HOME:-$HOME/.cache}/penecho/tmp"
+    mkdir -p "$TMPDIR"
+    chmod 700 "$TMPDIR"
+    exec ${penecho-unwrapped}/bin/penecho-unwrapped "$@"
+  '';
+
   orca-slicer-flatpak = pkgs.writeShellScriptBin "orca-slicer" ''
     exec ${pkgs.flatpak}/bin/flatpak run com.orcaslicer.OrcaSlicer "$@"
   '';
@@ -20,6 +59,7 @@ in {
   home.packages = with pkgs; [
     viddy
     orca-slicer-flatpak
+    penecho
     codex
     drawio
     gnome-sound-recorder
