40cb9bf79be179c08ed49df6b4a02cba6e95a104
Fernando Marques <offrakki@gmail.com>
2026-07-23 20:07:17 -0300

    fix impermanence parent ownership before activation

--- hosts/modules/optin-persistence.nix
+++ hosts/modules/optin-persistence.nix
@@ -25,14 +25,24 @@
     };
   };
 
-  system.activationScripts.persistent-dirs.text = let
-    mkHomePersist = user:
-      lib.optionalString user.createHome ''
-        mkdir -p /persist/${user.home}
-        chown ${user.name}:${user.group} /persist/${user.home}
-        chmod ${user.homeMode} /persist/${user.home}
+  system.activationScripts = {
+    persistent-dirs = {
+      deps = ["specialfs" "users" "groups"];
+      text = let
+        mkHomePersist = user:
+          lib.optionalString user.createHome ''
+            mkdir -p /persist/${user.home}
+            chown ${user.name}:${user.group} /persist/${user.home}
+            chmod ${user.homeMode} /persist/${user.home}
+          '';
+        users = lib.attrValues config.users.users;
+      in ''
+        install -d -m 0755 -o root -g root \
+          /persist /persist/etc /persist/home /persist/var /persist/var/lib /persist/var/log
+        ${lib.concatLines (map mkHomePersist users)}
       '';
-    users = lib.attrValues config.users.users;
-  in
-    lib.concatLines (map mkHomePersist users);
+    };
+
+    createPersistentStorageDirs.deps = ["persistent-dirs"];
+  };
 }
