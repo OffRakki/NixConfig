df22e7f03d13c201a7b41d5cd5313681273536f5
Fernando Marques <offrakki@gmail.com>
2026-08-01 20:40:48 -0300

    changed monitor hz

--- hosts/sora/home-manager/modules/hypr/hyprland.nix
+++ hosts/sora/home-manager/modules/hypr/hyprland.nix
@@ -152,7 +152,7 @@ in {
         -----------------------------------------------------------------------
 
         ------------------------------- MONITORS -------------------------------
-        hl.monitor({ output = "DP-1", mode = "highrr", position = "0x0", scale= 1 })
+        hl.monitor({ output = "DP-1", mode = "1920x1080@144", position = "0x0", scale= 1 })
         hl.monitor({ output = "DP-2", mode = "highres", position = "1920x0", scale= 1 })
         hl.monitor({ output = "HDMI-A-1", mode = "highres", position = "-1920x0", scale= 1 })
         ------------------------------------------------------------------------
