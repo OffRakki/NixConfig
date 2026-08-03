aae5c1da42a5cb7628bd6b3d68030807ebe156fd
Fernando Marques <offrakki@gmail.com>
2026-08-02 22:07:06 -0300

    workspace alocation

--- hosts/sora/home-manager/modules/hypr/hyprland.nix
+++ hosts/sora/home-manager/modules/hypr/hyprland.nix
@@ -159,7 +159,7 @@ in {
 
         ------------------------------- WORKSPACE RULES -------------------------------
         hl.workspace_rule({ workspace = "1", monitor = "DP-1", persistent = true, default = true })
-        hl.workspace_rule({ workspace = "2", monitor = "DP-2", persistent = true,})
+        hl.workspace_rule({ workspace = "2", monitor = "DP-1", persistent = true,})
         hl.workspace_rule({ workspace = "3", monitor = "DP-1", persistent = true,})
         hl.workspace_rule({ workspace = "4", monitor = "DP-1", persistent = true,})
         hl.workspace_rule({ workspace = "6", monitor = "HDMI-A-1", persistent = true,})
