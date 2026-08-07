0eb9f3ac57e7cf851182084b45328b75280618b1
Fernando Marques <offrakki@gmail.com>
2026-08-07 18:55:05 -0300

    toggled VRR off

--- hosts/sora/home-manager/modules/hypr/hyprland.nix
+++ hosts/sora/home-manager/modules/hypr/hyprland.nix
@@ -74,7 +74,7 @@ in {
           misc = {
             disable_hyprland_logo = true,
             disable_splash_rendering = true,
-            vrr = 1,
+            vrr = 0,
             mouse_move_enables_dpms = false,
             enable_swallow = false,
             swallow_regex = "^(kitty)$",
