4fb115651c363da51236589d6cb20fa8d10e8bf9
Fernando Marques <offrakki@gmail.com>
2026-08-07 18:55:05 -0300

    Added a Nix-shell LinOffice launcher

--- hosts/sora/home-manager/home-packages.nix
+++ hosts/sora/home-manager/home-packages.nix
@@ -100,6 +100,67 @@
     exec ${pkgs.flatpak}/bin/flatpak run com.orcaslicer.OrcaSlicer "$@"
   '';
 
+  linoffice = pkgs.writeShellScriptBin "linoffice" ''
+    export LINOFFICE_WRAPPER="$0"
+    exec nix shell --impure --expr '
+      let
+        pkgs = (builtins.getFlake "nixpkgs").legacyPackages.''${builtins.currentSystem};
+        python = pkgs.python3.withPackages (ps: [ps.pyside6 ps.python-dotenv]);
+      in
+      pkgs.buildEnv {
+        name = "linoffice-env";
+        paths = with pkgs; [
+          python
+          bash
+          coreutils
+          curl
+          findutils
+          freerdp
+          gawk
+          glib
+          gnugrep
+          gnused
+          gnutar
+          gzip
+          iproute2
+          iptables
+          jq
+          kmod
+          podman
+          podman-compose
+          procps
+          util-linux
+          xdg-utils
+        ];
+      }
+    ' -c bash -c '
+      set -euo pipefail
+      dir="$HOME/.local/bin/linoffice"
+
+      if [[ ! -f "$dir/gui/linoffice.py" ]]; then
+        tmp=$(mktemp -d)
+        trap "rm -rf \"$tmp\"" EXIT
+        curl -fsSL https://github.com/eylenburg/linoffice/archive/refs/heads/main.tar.gz |
+          tar -xz --strip-components=1 -C "$tmp"
+        mkdir -p "$(dirname "$dir")"
+        mv "$tmp" "$dir"
+        trap - EXIT
+      fi
+
+      find "$dir" -type f -name "*.sh" -exec sed -i "1s|^#!/bin/bash$|#!/usr/bin/env bash|" {} +
+      bash_path=$(command -v bash)
+      sed -i "s|self.process.setProgram(\"/bin/bash\")|self.process.setProgram(\"$bash_path\")|" \
+        "$dir/gui/installer/installer.py"
+      sed -i "s|^LINOFFICE=.*|LINOFFICE=\"$LINOFFICE_WRAPPER\"|" "$dir/setup.sh"
+
+      if (($#)); then
+        exec bash "$dir/linoffice.sh" "$@"
+      else
+        exec python3 "$dir/gui/linoffice.py"
+      fi
+    ' bash "$@"
+  '';
+
   winetricks-wrapped = pkgs.writeShellScriptBin "winetricks" ''
     # Resolve real ELF binaries from the wine wrapper's WINELOADER
     # so winetricks can detect the architecture (it reads ELF headers directly)
@@ -119,6 +180,7 @@ in {
     ab-download-manager
     viddy
     orca-slicer-flatpak
+    linoffice
     penecho
     codex
     drawio
