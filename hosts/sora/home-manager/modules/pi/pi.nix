e02a2ca676d72829eb01d25705d371d3cdece13f
Fernando Marques <offrakki@gmail.com>
2026-08-20 16:27:01 -0300

    Changed Pi theme to Sakura Macaron

--- hosts/sora/home-manager/modules/pi/pi.nix
+++ hosts/sora/home-manager/modules/pi/pi.nix
@@ -146,7 +146,7 @@ in {
       defaultProvider = "openai-codex";
       defaultModel = "gpt-5.6-sol";
       defaultThinkingLevel = "medium";
-      theme = "ciel-cursor";
+      theme = "sakura-macaron";
       enabledModels = [
         "gpt-5.5"
         "gpt-5.6*"
